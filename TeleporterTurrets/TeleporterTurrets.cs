04fc7a362927477975b9074ddf3acb59054e19a0
michael ross <Michael.ross@digipen.edu>
2020-12-25 16:50:02 -0800

    Add project files.

---
+++ TeleporterTurrets/TeleporterTurrets.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace RuneFoxMods
+{
+  [BepInDependency("com.bepis.r2api")]
+  //Change these
+  [BepInPlugin("com.RuneFoxMods.TeleporterTurrets", "TeleporterTurrets", "1.0.0")]
+  public class MyModName : BaseUnityPlugin
+  {
+    public void Awake()
+    {
+    }
+  }
+}
